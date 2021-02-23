c0995bb2276e91dd6260d2383ac65eb72e2fb2c4
José Peraza <joseperazarivero@gmail.com>
2021-02-22 20:53:51 -0600

    initial and final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
+import NavigationBar from "./components/NavigationBar.js";
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavigationBar />
+
+        <Switch>
+          <Route path="/about">Este es mi about</Route>
+          <Route path="/faq">Este es mi FAQ</Route>
+          <Route path="/store">Este es mi Store</Route>
+          <Route path="/">Este es mi Home</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
